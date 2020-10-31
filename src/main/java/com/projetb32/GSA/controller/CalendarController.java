38c1ba42b883ba38564cb5962386c2a2d7e15f43
pronetoussemagta <safraou.oussema@gmail.com>
2020-10-31 19:01:07 +0100

    change origin

--- src/main/java/com/projetb32/GSA/controller/CalendarController.java
+++ src/main/java/com/projetb32/GSA/controller/CalendarController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Date ;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/application")
 public class CalendarController  {
